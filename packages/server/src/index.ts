36e934c72c00ba83a4ed12659ed793db2c775008
Gareth <Gareth.kh.lau@gmail.com>
2021-02-27 15:20:52 -0700

    Use custom typeDefs and resolvers

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,24 +2,19 @@ import { ApolloServer } from "apollo-server";
 import { inferAugmentedSchema, driver } from "./neo4j";
 import { PORT } from "./config";
 import { GraphQLSchema } from "graphql";
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
 
-const createServer = (augmentedSchema: GraphQLSchema) =>
-  new ApolloServer({
-    schema: augmentedSchema,
-    // inject the request object into the context to support middleware
-    // inject the Neo4j driver instance to handle database call
-    context: ({ req }) => {
-      return {
-        driver,
-        req,
-      };
-    },
-  });
-
-inferAugmentedSchema(driver)
-  .then(createServer)
-  .then((server: ApolloServer) => server.listen(PORT, "0.0.0.0"))
-  .then(({ url }: { url: string }) => {
-    console.log(`GraphQL API ready at ${url}`);
-  })
-  .catch((err: Error) => console.error(err));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    return {
+      driver,
+      req,
+    };
+  },
+});
+server.listen(PORT).then(({ url }: { url: string }) => {
+  console.log(`GraphQL API ready at ${url}`);
+});
